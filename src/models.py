30f29c15f7ee3a101aa5aea5ba5170e6f8d3f57f
sergiopicado <sergiopicado@gmail.com>
2021-02-26 17:59:53 +0000

    Finalizado

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,67 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Basic_Characters(Base):
+    __tablename__ = 'Basic_Characters'
+    uid = Column(Integer, primary_key=True)
+    name = Column(String(200))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Properties_Characters(Base):
+    __tablename__ = 'Properties_Characters'
+    id = Column(Integer, primary_key=True) 
+    uid = Column(Integer, ForeignKey('Basic_Characters.uid'))
+    height = Column(Float)
+    mass = Column(Integer)
+    hair_color = Column(String(50))
+    skin_color = Column(String(50))
+    eye_color = Column(String(50))
+    birth_year = Column(String(50))
+    gender = Column(String(50))
+    created = Column(DateTime)
+    edited = Column(DateTime)
+    homeworld = Column(Integer, ForeignKey('Basic_Characters.uid'))
+    description = Column(String(250))
+    url = Column(String(250))
+
+class Basic_Planets(Base):
+    __tablename__ = 'Basic_Planets'
+    uid = Column(Integer, primary_key=True)
+    name = Column(String(200))
+
+class Properties_Planets(Base):
+    __tablename__ = 'Properties_Planets'
+    id = Column(Integer, primary_key=True) 
+    uid = Column(Integer, ForeignKey('Basic_Planets.uid'))
+    diameter = Column(Float)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(Float)
+    population = Column(Integer)
+    climate = Column(String(50))
+    terrain = Column(String(50))
+    surface_water = Column(Integer)
+    created = Column(DateTime)
+    edited = Column(DateTime)
+    url = Column(String(250))  
+
+class User_Profile(Base):
+    __tablename__ = 'User_Profile'
+    uid = Column(Integer, primary_key=True)
+    name = Column(String(200)) 
+
+class Detail_Profile(Base):
+    __tablename__ = 'Detail_Profile'
+    id = Column(Integer, primary_key=True) 
+    uid = Column(Integer, ForeignKey('User_Profile.uid'))
+    age = Column(Integer)
+    nation = Column(String(80))
+
+class User_Favorites(Base):
+    __tablename__ = 'User_Favorites'
+    id = Column(Integer, primary_key=True) 
+    uid_User = Column(Integer, ForeignKey('User_Profile.uid'))
+    uid_Character = Column(Integer, ForeignKey('Basic_Characters.uid'))
+    uid_Planets = Column(Integer, ForeignKey('Basic_Planets.uid'))
 
     def to_dict(self):
         return {}
